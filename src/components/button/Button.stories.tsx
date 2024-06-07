3a32ecc5191a9a3fbb4c011f17fda805a2a726fe
trixpo <ploxhat@gmail.com>
2024-06-07 09:45:46 -0600

    Add Button component

---
+++ src/components/button/Button.stories.tsx
@@ -0,0 +1,20 @@
+import {Meta, StoryObj} from '@storybook/react'
+import Button from './Button'
+
+const meta: Meta<typeof Button> = {
+    component: Button,
+}
+export default meta
+type Story = StoryObj<typeof Button>
+
+export const Primary: Story = {
+    args: {
+        label: 'Primary',
+    },
+}
+
+export const Secondary: Story = {
+    args: {
+        label: 'Secondary',
+    },
+}
