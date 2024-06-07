24f4b9f2f46505045247928a92cb438c9fdff8af
trixpo <ploxhat@gmail.com>
2024-06-07 10:25:34 -0600

    Add a story for the Rating component

---
+++ src/components/rating/Rating.stories.tsx
@@ -0,0 +1,22 @@
+import { StoryFn, Meta } from "@storybook/react"
+import Rating from "./Rating"
+
+export default {
+  component: Rating,
+} as Meta<typeof Rating>
+
+const Template: StoryFn<typeof Rating> = (args) => <Rating {...args} />
+
+export const RatingTest = Template.bind({});
+RatingTest.args = {
+  title: "Default theme",
+  theme: "primary",
+  testIdPrefix: "rating",
+}
+
+export const RatingSecondary = Template.bind({});
+RatingSecondary.args = {
+  title: "Secondary theme",
+  theme: "secondary",
+  testIdPrefix: "rating",
+}
